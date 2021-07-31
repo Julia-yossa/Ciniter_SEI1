de886f25fada80da2d9ddaa4f353d02db57cb2c8
C. Yossa Julia <durinelle16.jl@gmail.com>
2021-07-31 20:12:11 +0100

    complete the product exercise

--- src/product.java
+++ src/product.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class product {
-    public void main
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int number1;
+        int number2;
+        int number3;
+        int answer;
+
+        System.out.print("Enter a number: ");
+        number1 = input.nextInt();
+
+        System.out.print("Enter a number: ");
+        number2 = input.nextInt();
+
+        System.out.print("Enter a number: ");
+        number3 = input.nextInt();
+
+        answer = number1 * number2 * number3;
+
+        System.out.printf("The product of %d, %d and %d is %d%n.", number1, number2, number3, answer);
+    }
 }
